70327f4ffe14ab32d5507d9a7128b62c8c58af13
Thomas Dziedzic <gostrc@gmail.com>
2013-04-28 17:07:23 -0700

    fix warnings: add top level type signatures

--- tests/tests.hs
+++ tests/tests.hs
@@ -3,27 +3,37 @@ import Test.HUnit
 import Package.Types
 import Package.Library
 
+ghc :: String
 ghc = "ghc=7.6.3-1"
+
+testArch :: String
 testArch = "x86_64"
 
+pkgTransformers :: PkgDesc
 pkgTransformers = PkgDesc "haskell-transformers" "transformers" [0,3,0,0] 4 [ghc, "sh"]
+
+pkgMtl :: PkgDesc
 pkgMtl = PkgDesc "haskell-mtl" "mtl" [2,1,2] 3 [ghc, "sh", "haskell-transformers"]
 
+testPkgs :: [PkgDesc]
 testPkgs =
     [ pkgTransformers
     , pkgMtl
     ]
 
+getDependencyStringTest0 :: Test
 getDependencyStringTest0 = TestCase $ assertEqual
     "the dependency string should contain the transformers package"
     " -- -I ../../haskell-transformers/trunk/haskell-transformers-0.3.0.0-4-x86_64.pkg.tar.xz"
     (getDependencyString testPkgs pkgMtl testArch)
 
+getDependencyStringTest1 :: Test
 getDependencyStringTest1 = TestCase $ assertEqual
     "the dependency string should not contain any packages"
     ""
     (getDependencyString testPkgs pkgTransformers testArch)
 
+main :: IO Counts
 main = runTestTT $ TestList
     [ getDependencyStringTest0
     , getDependencyStringTest1
